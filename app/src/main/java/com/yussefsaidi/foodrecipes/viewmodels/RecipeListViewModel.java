c5518bd75e7fde84b4490a60ae4c95c2ffddd9d3
Yussef <saidi.yussef@live.fr>
2020-03-24 15:03:05 -0400

    Finish setting up base project

--- app/src/main/java/com/yussefsaidi/foodrecipes/viewmodels/RecipeListViewModel.java
+++ app/src/main/java/com/yussefsaidi/foodrecipes/viewmodels/RecipeListViewModel.java
@@ -5,18 +5,15 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.ViewModel;
 
 
-public class RecipeListViewModel extends AndroidViewModel {
+public class RecipeListViewModel extends ViewModel {
 
     private static final String TAG = "RecipeListViewModel";
-
-    public RecipeListViewModel(@NonNull Application application) {
-        super(application);
-
+    
+    public RecipeListViewModel() {
     }
-
-
 }
 
 
