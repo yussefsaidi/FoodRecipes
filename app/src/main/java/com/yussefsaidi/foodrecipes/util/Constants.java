ca3b4a6e2b61736fc49011ea0b5689ac95786190
Yussef <saidi.yussef@live.fr>
2020-03-28 15:23:28 -0400

    Change API url

--- app/src/main/java/com/yussefsaidi/foodrecipes/util/Constants.java
+++ app/src/main/java/com/yussefsaidi/foodrecipes/util/Constants.java
@@ -2,18 +2,14 @@ package com.yussefsaidi.foodrecipes.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://www.food2fork.com";
-
-    // YOU NEED YOUR OWN API KEY!!!!!!!!!!!!! https://www.food2fork.com/about/api
-    public static final String API_KEY = "dadc63b6325aaf398163b40fea9b5e79";
-
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com/";
 
     public static final String[] DEFAULT_SEARCH_CATEGORIES =
-            {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
+            {"Barbecue", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
 
     public static final String[] DEFAULT_SEARCH_CATEGORY_IMAGES =
             {
-                    "barbeque",
+                    "barbecue",
                     "breakfast",
                     "chicken",
                     "beef",
