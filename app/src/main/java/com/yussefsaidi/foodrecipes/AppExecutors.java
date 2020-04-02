ef7ec7706f5381c1249e78c6e70d38ad8d619782
Yussef <saidi.yussef@live.fr>
2020-04-02 15:26:30 -0400

    Handle retrofit requests with local cache

--- app/src/main/java/com/yussefsaidi/foodrecipes/AppExecutors.java
+++ app/src/main/java/com/yussefsaidi/foodrecipes/AppExecutors.java
@@ -1,8 +1,11 @@
 package com.yussefsaidi.foodrecipes;
 
 
+import android.os.Handler;
+import android.os.Looper;
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
+
 
 public class AppExecutors {
 
@@ -15,9 +18,24 @@ public class AppExecutors {
         return instance;
     }
 
-    private final ScheduledExecutorService mNetworkIO = Executors.newScheduledThreadPool(3);
+    private final Executor mDiskIO = Executors.newSingleThreadExecutor();
 
-    public ScheduledExecutorService networkIO(){
-        return mNetworkIO;
+    private final Executor mMainThreadExecutor = new MainThreadExecutor();
+
+    public Executor diskIO(){
+        return mDiskIO;
+    }
+
+    public Executor mainThread(){
+        return mMainThreadExecutor;
+    }
+
+    private static class MainThreadExecutor implements Executor{
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable runnable) {
+            mainThreadHandler.post(runnable);
+        }
     }
 }
